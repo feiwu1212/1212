46d614920965080e14fb8507faec26d83ccd861e
但锐轩 <dandancc007@qq.com>
2018-01-08 10:58:04 +0800

    测试提交

--- cdg-parent/cdg-gateway-core/src/main/java/com/crfchina/cdg/core/dto/base/BaseParamDTO.java
+++ cdg-parent/cdg-gateway-core/src/main/java/com/crfchina/cdg/core/dto/base/BaseParamDTO.java
@@ -7,9 +7,9 @@
  */
 package com.crfchina.cdg.core.dto.base;
 
-import com.crfchina.csf.enums.SystemNo;
+import com.crfchina.cdg.common.enums.common.SystemNo;
 
-/**    
+/**
  * 
  * @ProjectName：cdg-service
  * @ClassName：BaseParamDTO 

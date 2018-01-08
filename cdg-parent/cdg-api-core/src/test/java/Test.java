116a215e39117a06a884fde389120e3805e68129
但锐轩 <dandancc007@qq.com>
2018-01-08 17:32:50 +0800

    测试目录

---
+++ cdg-parent/cdg-api-core/src/test/java/Test.java
@@ -0,0 +1,19 @@
+/**
+ * @Title：
+ * @Package PACKAGE_NAME
+ * @date 2018/1/8 17:32
+ * @version V1.0
+ */
+
+/**
+ * @ProjectName：cdg-parent
+ * @ClassName：Test
+ * @Description:
+ * @author: Administrator
+ * @date：2018/1/8 17:32
+ * @updateBy：但锐轩
+ * @updateDate：2018/1/8 17:32
+ * @remarks：
+ */
+public class Test {
+}

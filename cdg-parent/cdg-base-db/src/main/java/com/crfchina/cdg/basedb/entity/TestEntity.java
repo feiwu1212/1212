b912873ca030232c938ebd4fbe39e99b8919d97f
但锐轩 <dandancc007@qq.com>
2018-01-08 17:48:39 +0800

    mybatis

---
+++ cdg-parent/cdg-base-db/src/main/java/com/crfchina/cdg/basedb/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * @Title：
+ * @Package com.crfchina.cdg.basedb.entity
+ * @date 2018/1/8 12:11
+ * @version V1.0
+ */
+package com.crfchina.cdg.basedb.entity;
+
+/**
+ * @ProjectName：cdg-parent
+ * @ClassName：TestEntity
+ * @Description:
+ * @author: Administrator
+ * @date：2018/1/8 12:11
+ * @updateBy：但锐轩
+ * @updateDate：2018/1/8 12:11
+ * @remarks：
+ */
+public class TestEntity {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}

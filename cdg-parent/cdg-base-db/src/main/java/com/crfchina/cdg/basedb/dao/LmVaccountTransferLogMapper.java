6f567ea783a59bcf40b4e198cc47a164642ea65d
但锐轩 <dandancc007@qq.com>
2018-01-15 10:19:09 +0800

    增加新表

---
+++ cdg-parent/cdg-base-db/src/main/java/com/crfchina/cdg/basedb/dao/LmVaccountTransferLogMapper.java
@@ -0,0 +1,24 @@
+package com.crfchina.cdg.basedb.dao;
+
+import com.crfchina.cdg.basedb.entity.LmVaccountTransferLog;
+import com.crfchina.cdg.basedb.entity.LmVaccountTransferLogExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface LmVaccountTransferLogMapper {
+    int countByExample(LmVaccountTransferLogExample example);
+
+    int deleteByPrimaryKey(@Param("id") Long id, @Param("partitionDate") Integer partitionDate);
+
+    int insert(LmVaccountTransferLog record);
+
+    int insertSelective(LmVaccountTransferLog record);
+
+    List<LmVaccountTransferLog> selectByExample(LmVaccountTransferLogExample example);
+
+    LmVaccountTransferLog selectByPrimaryKey(@Param("id") Long id, @Param("partitionDate") Integer partitionDate);
+
+    int updateByPrimaryKeySelective(LmVaccountTransferLog record);
+
+    int updateByPrimaryKey(LmVaccountTransferLog record);
+}

85bddb7200cf51411129847b3469ad779f5d09cf
但锐轩 <dandancc007@qq.com>
2018-01-23 09:33:36 +0800

    notify添加缓存

---
+++ cdg-parent/cdg-notify/src/main/java/com/crfchina/cdg/notify/service/LmCacheService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.crfchina.cdg.notify.service;
+
+
+/**
+ * @author ghf
+ *
+ */
+public interface LmCacheService {
+
+	String getLmBankCode(String bankCode);
+	
+	String getBankCode(String lmBankCode);
+}
